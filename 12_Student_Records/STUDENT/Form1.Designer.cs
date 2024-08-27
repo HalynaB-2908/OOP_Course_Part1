03fa7ba2246752f85ecddba541fdd52efd6d1e46
HalynaB <galja2908@gmail.com>
2024-08-28 00:30:44 +0200

    task 12 added

---
+++ 12_Student_Records/STUDENT/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Student_Records
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Surname";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(285, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Exam 1\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(450, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Exam 2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(624, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Exam 3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(803, 69);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label5";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(100, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 26);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(353, 66);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(76, 26);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(523, 66);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(83, 26);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(706, 66);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(75, 26);
+            this.textBox4.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 57);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "add Student label";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(196, 125);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(204, 54);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "add Student message box";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(598, 127);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(163, 52);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Add Student to arr";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(416, 126);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(168, 52);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "New arr of Students";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(24, 215);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(915, 348);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(777, 127);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(162, 52);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Max average score";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(972, 611);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ 12_Student_Records/STUDENT/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Student_Records
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Student class definition
+        class Student
+        {
+            public string LastName; // Student's last name
+            private int exam1, exam2, exam3; // Exam scores
+
+            public double AvgScore // Average score property
+            {
+                get { return (exam1 + exam2 + exam3) / 3.0; }
+            }
+
+            // Exam1 property with validation
+            public int Exam1
+            {
+                set { if (value > 0 && value <= 5) exam1 = value; }
+                get { return exam1; }
+            }
+
+            // Exam2 property with validation
+            public int Exam2
+            {
+                set { if (value > 0 && value <= 5) exam2 = value; }
+                get { return exam2; }
+            }
+
+            // Exam3 property with validation
+            public int Exam3
+            {
+                set { if (value > 0 && value <= 5) exam3 = value; }
+                get { return exam3; }
+            }
+
+            // Default constructor
+            public Student() { }
+
+            // Constructor with parameters
+            public Student(int x1, int x2, int x3)
+            {
+                exam1 = x1;
+                exam2 = x2;
+                exam3 = x3;
+            }
+        }
+
+        // Button click event to display student's average score
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Student stud = new Student();
+            stud.LastName = textBox1.Text;
+            stud.Exam1 = Convert.ToInt32(textBox2.Text);
+            stud.Exam2 = Convert.ToInt32(textBox3.Text);
+            stud.Exam3 = Convert.ToInt32(textBox4.Text);
+            label5.Text = stud.LastName + " " + stud.AvgScore.ToString("F2"); // Display average score with 2 decimal places
+        }
+
+        Student studWithParams;
+
+        // Button click event to create a student with provided parameters and display their average score
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int x1 = Convert.ToInt32(textBox2.Text);
+            int x2 = Convert.ToInt32(textBox3.Text);
+            int x3 = Convert.ToInt32(textBox4.Text);
+            studWithParams = new Student(x1, x2, x3);
+            studWithParams.LastName = textBox1.Text;
+            MessageBox.Show(studWithParams.LastName + " " + studWithParams.AvgScore.ToString("F2"));
+        }
+
+        Student[] studArray;
+        int currStudInd = 0;
+        int maxStudCount = 10;
+
+        // Button click event to initialize the students array and configure DataGridView
+        private void button4_Click(object sender, EventArgs e)
+        {
+            studArray = new Student[maxStudCount];
+            dataGridView1.RowCount = maxStudCount;
+            dataGridView1.ColumnCount = 5;
+            dataGridView1.Columns[0].HeaderText = "Last Name";
+            dataGridView1.Columns[1].HeaderText = "Exam 1";
+            dataGridView1.Columns[2].HeaderText = "Exam 2";
+            dataGridView1.Columns[3].HeaderText = "Exam 3";
+            dataGridView1.Columns[4].HeaderText = "Avg Score";
+            dataGridView1.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill; 
+            dataGridView1.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells; 
+            dataGridView1.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            dataGridView1.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            dataGridView1.Columns[4].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+        }
+
+        // Button click event to add a student to the array and display in DataGridView
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Student stud = new Student();
+            stud.LastName = textBox1.Text;
+            stud.Exam1 = Convert.ToInt32(textBox2.Text);
+            stud.Exam2 = Convert.ToInt32(textBox3.Text);
+            stud.Exam3 = Convert.ToInt32(textBox4.Text);
+            studArray[currStudInd] = stud;
+            dataGridView1.Rows[currStudInd].Cells[0].Value = studArray[currStudInd].LastName;
+            dataGridView1.Rows[currStudInd].Cells[1].Value = studArray[currStudInd].Exam1;
+            dataGridView1.Rows[currStudInd].Cells[2].Value = studArray[currStudInd].Exam2;
+            dataGridView1.Rows[currStudInd].Cells[3].Value = studArray[currStudInd].Exam3;
+            dataGridView1.Rows[currStudInd].Cells[4].Value = studArray[currStudInd].AvgScore.ToString("F2");
+            currStudInd++;
+        }
+
+        // Button click event to find and display the student with the highest average score
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (studArray != null && currStudInd > 0)
+            {
+                Student topStud = studArray[0];
+
+                for (int i = 1; i < currStudInd; i++)
+                {
+                    if (studArray[i].AvgScore > topStud.AvgScore)
+                        topStud = studArray[i];
+                }
+                MessageBox.Show("Student with the highest average score: " + topStud.LastName + ", Score: " + topStud.AvgScore.ToString("F2"));
+            }
+            else
+                MessageBox.Show("The student array is empty.");
+        }
+    }
+}
