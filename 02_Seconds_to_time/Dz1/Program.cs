af7cf1bf9c7bb88c6fd3bd6afcc7752824477fbb
HalynaB <galja2908@gmail.com>
2024-08-27 16:37:09 +0200

    task 2 - seconds to time

---
+++ 02_Seconds_to_time/Dz1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SecondsToTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number of seconds: ");
+            int seconds = Convert.ToInt32(Console.ReadLine());
+
+            int hours = seconds / 3600;
+            int restSeconds = seconds % 3600; 
+
+            int minutes = restSeconds / 60; 
+            int remainingSeconds2 = restSeconds % 60; 
+
+            Console.WriteLine(seconds + " seconds - is " + hours + " hour " + minutes + " minutes " + remainingSeconds2 + " seconds");
+        }
+    }
+}
+
